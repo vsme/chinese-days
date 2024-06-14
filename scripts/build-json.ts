7a6fe13ea521674d1460b4f72db996f3d97b2ee0
yaavi <27679365@qq.com>
2024-06-15 02:28:54 +0800

    feat: 增加ics日历文件

--- scripts/build-json.ts
+++ scripts/build-json.ts
@@ -4,5 +4,5 @@ import generate from '../src/holidays/generate';
 // 保存到 ./dist/chinese-days.json 文件
 fs.writeFile("./dist/chinese-days.json", JSON.stringify(generate()), (err) => {
   if (err) throw err;
-  console.log("The json file has been saved!");
+  console.log("The JSON file has been saved!");
 });
